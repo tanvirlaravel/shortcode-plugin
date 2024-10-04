8e52a0dac78161d921a74e9de3f57e0d461ba579
Tanvir <tanvirlaravel@gmail.com>
2024-10-04 22:35:36 +0600

    initail commit

---
+++ shortcode-plugin.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: ShortCode Plugin
+ * Description: This is second plugin of the course
+ * Author: Md Tanvirul Islam
+ * Version: 1.0.0
+ * Author URI: www.tanvir.com
+ * Plugin URI: https/example.com/hello-world
+ */

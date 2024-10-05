ec2592d78ea81100543c9fb83d830205f23a63ac
Tanvir <tanvirlaravel@gmail.com>
2024-10-05 13:21:12 +0600

    basic shortcode plugin done

--- shortcode-plugin.php
+++ shortcode-plugin.php
@@ -7,3 +7,12 @@
  * Author URI: www.tanvir.com
  * Plugin URI: https/example.com/hello-world
  */
+
+//  this is short code 
+//  [message]
+
+add_shortcode("message", "sp_show_static_message");
+
+function sp_show_static_message() {
+    return '<p style="color:red;font-weight:bold;font-size:36px;">Hello i am a simple message</p>';
+}

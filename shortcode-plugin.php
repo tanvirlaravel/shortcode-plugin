ea8930c1eb0edeff8b70fc58fed341abbbb2f504
Tanvir <tanvirlaravel@gmail.com>
2024-10-05 14:11:11 +0600

    Shortcode with db operation

--- shortcode-plugin.php
+++ shortcode-plugin.php
@@ -56,4 +56,48 @@ function sp_handle_student_data( $attributes ) {
     return "<h3>Student data: Name: " . $attributes['name'] . "  Email: " . $attributes['email'] . "</h3>";
 }
 
- 
+ /** *****************************************************************
+  *                        Shortcode with db operation
+ *********************************************************************/ 
+add_shortcode("list-posts", "sp_handle_list_posts");
+
+function sp_handle_list_posts(){
+    /*
+    The $wpdb global variable in WordPress is a crucial object that provides access to the database. It encapsulates methods for interacting with the WordPress database, making it easier to perform various database operations.
+
+    Key Functions and Properties:
+    $wpdb->prepare(): This function prepares SQL statements, preventing SQL injection vulnerabilities by properly escaping and formatting data.
+    $wpdb->query(): Executes a prepared SQL statement and returns the number of rows affected or the result set.
+    $wpdb->get_var(): Retrieves a single value from the database.
+    $wpdb->get_row(): Fetches a single row from the database as an object.
+    $wpdb->get_results(): Retrieves multiple rows from the database as an array of objects.
+    $wpdb->insert(): Inserts a new row into the database.
+    $wpdb->update(): Updates an existing row in the database.
+    $wpdb->delete(): Deletes a row from the database.
+    $wpdb->show_errors(): Enables or disables error display.
+    */
+    global $wpdb;
+
+    $table_prefix = $wpdb->prefix; // wp_
+    $table_name = $table_prefix . "posts"; // wp_posts
+
+    // Get posts where post type = post and post status = published
+    $posts = $wpdb->get_results(
+        "SELECT post_title FROM {$table_name} WHERE post_type='post' AND post_status='publish' "
+    );
+
+   if( count($posts) > 0 ){
+
+    $outputHtml = "<ul>";
+
+    foreach( $posts as $post ){
+        $outputHtml .= "<li>" . $post->post_title . "</li>";
+    }
+
+    $outputHtml .= "</ul>";
+
+    return $outputHtml;
+   }
+
+   return "<h3>No post found</h3>";
+}
